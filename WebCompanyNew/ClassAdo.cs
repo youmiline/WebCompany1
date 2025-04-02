087b628284c76b5b300769996fe02de48b556bdb
youmiline <youmiline@mail.ru>
2025-04-02 13:05:15 +0300

    Добавьте файлы проекта.

---
+++ WebCompanyNew/ClassAdo.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebCompanyNew
+{
+	public class ClassAdo
+	{
+		string connectionString;
+
+		public ClassAdo()
+		{
+			connectionString = "Data Source=LAPTOP-NL88HAUL\\NITRO_SQL_SERVER;Initial Catalog=session;Integrated Security=True;" +
+				"Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		}
+
+		public DataSet GetDataSet(string sqlQuery)
+		{
+			SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, connectionString);
+			DataSet ds = new DataSet();
+			adapter.Fill(ds,"Table");
+			return ds;
+		}
+	}
+}
